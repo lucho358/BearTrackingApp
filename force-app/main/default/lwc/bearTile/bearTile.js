d113bcbae815a65594d95eef7c9883a7a5ec79d1
lmiraballes <lfmmartino@gmail.com>
2023-06-22 20:31:24 -0700

    create a child component and interact with it

--- force-app/main/default/lwc/bearTile/bearTile.js
+++ force-app/main/default/lwc/bearTile/bearTile.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
-
-export default class BearTile extends LightningElement {}
+import { LightningElement, api } from 'lwc';
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+export default class BearTile extends LightningElement {
+	@api bear;
+	appResources = {
+		bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
+	};
+    handleOpenRecordClick() {
+        const selectEvent = new CustomEvent('bearview', {
+            detail: this.bear.Id
+        });
+        this.dispatchEvent(selectEvent);
+    }
+}
